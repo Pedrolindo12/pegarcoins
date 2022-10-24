59f7622e8cb6a206ec399f99b4c2f2ea75aff2dc
Pedrolindo12 <116006178+Pedrolindo12@users.noreply.github.com>
2022-10-24 09:32:59 -0300

    Add files via upload

--- Assets/Scripts/MoveAviao.cs
+++ Assets/Scripts/MoveAviao.cs
@@ -30,13 +30,13 @@ public class MoveAviao : MonoBehaviour
             transform.Translate(new Vector3(0,0.02f,0));
         }
         if(Input.GetKey(KeyCode.DownArrow)){
-            transform.Translate(new Vector3(0,-0.2f,0));
+            transform.Translate(new Vector3(0,-0.02f,0));
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(new Vector3(-0.2f,0,0));
+            transform.Translate(new Vector3(-0.02f,0,0));
         }
         if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(new Vector3(0.2f,0,0));
+            transform.Translate(new Vector3(0.02f,0,0));
         }
         print("foram coletadas"+moedasColetadas);
 
