309321049578d4e26e68b147721fc0eeccfea0e4
Pedrolindo12 <116006178+Pedrolindo12@users.noreply.github.com>
2022-10-24 11:10:22 -0300

    Add files via upload

---
+++ Assets/Scripts/Movetiro.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movetiro : MonoBehaviour
+{
+    
+    public float velocidade = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(velocidade*Time.deltaTime,0));
+    }
+}
